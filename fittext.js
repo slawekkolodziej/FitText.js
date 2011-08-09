c755b680b9d759247c02d94078e42659e121924a
Slawomir Kolodziej <s@slawekk.info>
2011-08-09 10:55:23 +0200

    Cleaned up the code

--- fittext.js
+++ fittext.js
@@ -7,47 +7,46 @@
 * Modified by Slawomir Kolodziej http://slawekk.info
 * @todo: add IE support
 *
-* Date: Thu May 05 14:23:00 2011 -0600
+* Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
 */
 (function(){
   var css = function (el, prop) {
-	  return window.getComputedStyle ? getComputedStyle(el).getPropertyValue(prop) : el.currentStyle[prop];
-  }
+    return window.getComputedStyle ? getComputedStyle(el).getPropertyValue(prop) : el.currentStyle[prop];
+  };
   
-	var	addEvent = function(el, type, fn){
-		if(el.addEventListener)
-			el.addEventListener(type, fn, false);
-			
+  var addEvent = function (el, type, fn) {
+    if (el.addEventListener)
+      el.addEventListener(type, fn, false);
 		else
 			el.attachEvent('on'+type, fn);
-	};
+  };
 
-	window.fitText = function(el, kompressor) {
-	
-			var fit = function (el) {
-				var origFontSize = parseFloat( css(el, 'font-size') ),
-						compressor = kompressor || 1;
-				
-				var resizer = function () {	
-					el.style.fontSize = Math.min(el.clientWidth / (compressor*10), origFontSize) + 'px';
-				};
+  window.fitText = function (el, kompressor) {
 
-				// Call once to set.
-				resizer();
+    var fit = function (el) {
+      var origFontSize = parseFloat( css(el, 'font-size') ),
+          compressor = kompressor || 1;
 
-				// Bind events
-				// If you have any js library which support Events, replace this part
-				// and remove addEvent function (or use original jQuery version)
-				addEvent(window, 'resize', resizer);
-			}
-			
-			if (el.length) 
-				for(var i=0; i<el.length; i++) 
-					fit(el[i])
-			else
-				fit(el);
-			
-			// return set of elements
-			return el;	
-	}
+      var resizer = function () {
+        el.style.fontSize = Math.min(el.clientWidth / (compressor * 10), origFontSize) + 'px';
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Bind events
+      // If you have any js library which support Events, replace this part
+      // and remove addEvent function (or use original jQuery version)
+      addEvent(window, 'resize', resizer);
+    };
+
+    if (el.length)
+      for(var i=0; i<el.length; i++)
+        fit(el[i]);
+    else
+      fit(el);
+
+    // return set of elements
+    return el;
+  };
 })();
